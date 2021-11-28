f56eb8657dabe9c86a16da935f2e52c8212576cb
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-11-28 11:56:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,12 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith("tree "):
-        gds = [x.name for x in client.guilds]
         msg = f"I\'m currently under-development , <@{message.author.id}> \n please try again later \n Your message content was \n ```{message.content}```\n"  
-        await message.channel.send(gds)
         await message.channel.send(msg)
+
+    if message.content.startswith("branches "):
+        gds = [x.name for x in client.guilds]
+        await message.channel.send( "\n".join(gds) )
 
 # Actual start logging-in
 client.run(TOKEN)
