683309490726677b41cbe3a28d211860f389461b
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-11-30 06:02:19 +0200

    Update main.py
    Test two :
    Testing joining and leaving again.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ client = discord.Client()
 
 # Prefix of the bot
 pfx = "."
+cmds = {0:"join",1:"leave"}
 
 # A decorator function to start
 @client.event
@@ -35,7 +36,7 @@ async def on_message(message):
         await message.add_reaction(emoji[2])
         await message.add_reaction(emoji[3])
 
-    elif msg == pfx+pfx:
+    elif msg == pfx+cmds[1]:
         if message.guild.voice_client is None:
            await message.channel.reply("I'm not even in a voice channel")
            return
@@ -43,22 +44,25 @@ async def on_message(message):
            await message.guild.voice_client.disconnect()
            await message.add_reaction(emoji[1])
            await message.reply("See you later")
-
-    elif msg.startswith(pfx) and len(msg) > 1 :
-        rpl = f"I\'m currently under-development , {author} \n please try again later \n Your message content was \n ```{message.content}```\n"  
-        await message.reply(rpl)
-        await message.add_reaction(emoji[1])
+    
+    elif msg == pfx+cmds[0]:
         if author.voice is None:
            await message.reply("You are not in a **Voice Channel**")
            return
         else:
            if author.voice.channel.id != message.voice_client.channel.id:
               message.guild.voice_client.move_to(message.author.voice.channel)
+              message.reply(f"Wait for me , I'm comming to {author.voice.channel.name}\n")
            else:
               await author.voice.channel.connect()
               await message.add_reaction(emoji[4])
               await message.add_reaction(emoji[1])
               await message.reply("**I'm Connecting...**",mention_author=False)
+
+    elif msg.startswith(pfx) and msg[1:] not in cmds :
+        rpl = f"I\'m currently under-development , {author} \n please try again later \n Your message content was \n ```{message.content}```\n"  
+        await message.reply(rpl)
+        await message.add_reaction(emoji[1])
      
 
 # Actual start logging-in
