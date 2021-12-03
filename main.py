2623f1be2d00bddff84d3a18e67cfffc14ed556c
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-03 07:51:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def on_message(message):
 
   if message.guild is None:
     embed=discord.Embed(title="Invite me to your **server**", url="https://discordapp.com/oauth2/authorize?client_id=569724616210382875&scope=bot&permissions=277129284672", description="", color=0x00ff00)
-    embed.set_author(name=f"{message.author.name}", url="https://discordapp.com/oauth2/authorize?client_id=569724616210382875&scope=bot&permissions=277129284672", icon_url=f"{message.author.display_avatar}")
+    embed.set_author(name=f"{message.author}", url="https://discordapp.com/oauth2/authorize?client_id=569724616210382875&scope=bot&permissions=277129284672", icon_url=f"{message.author.display_avatar}")
     embed.set_thumbnail(url=client.user.display_avatar)
     embed.set_image(url=client.user.display_avatar)
     embed.set_footer(text=f"I'm a nice bot under construction")
