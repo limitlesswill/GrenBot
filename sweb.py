cf240359a28c561d9d5d96bca368ab91afbaf23e
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 01:07:15 +0200

    Update sweb.py
    adding more exception details error codes.

--- sweb.py
+++ sweb.py
@@ -6,9 +6,9 @@ class hweb:
     self.host = url
     self.port = 80
     self.request = b"GET / HTTP/1.0\r\n\r\n"
-    self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.ip = socket.gethostbyname(self.host)
     try:
+      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.ip = socket.gethostbyname(self.host)
       self.s.connect((self.ip , self.port))
     except:
       result = "connection error"
