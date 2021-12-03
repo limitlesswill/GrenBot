99148d72a72c7ed9c297248271024ddaee7cb03b
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-03 02:37:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,14 @@ async def on_ready():
 # A decorator function to read message the send response
 @client.event
 async def on_message(message):
+  if not message.guild:
+    dmsg = f"**Invite me to a server so we can play\nhttps://discordapp.com/oauth2/authorize?client_id=569724616210382875&scope=bot&permissions=277129284672**"
+    await message.reply(dmsg)
+    await message reply(message.author.displayAvatarURL)
+    return
+
   msg = message.content.lower()
+  cmd = msg.startswith(pfx)
   author = message.author
   emoji = ["👀","👋","👉","👈","👍"]
 
@@ -37,13 +44,13 @@ async def on_message(message):
     await message.add_reaction(emoji[2])
     await message.add_reaction(emoji[3])
 
-  if msg[len(pfx):] not in cmds and len(msg[len(pfx):]) > 1:
+  if cmd and msg[len(pfx):] not in cmds:
     rpl = f"**I'm currently under-development**,{author}"
     global count
     count += 1
     await message.reply(rpl+f"\ncount **{str(count)}**")
 
-  if msg[len(pfx):] == cmds[2]:
+  if cmd and msg[len(pfx):] == cmds[2]:
     try:
       obj = hweb()
       txt = obj.get_data()
