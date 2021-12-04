d222e6e7fc36e8dc31f2191092c2d6eeb41f7e7f
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 22:15:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ async def on_message(message):
       obj = hweb()
       await message.reply(f"initializing hweb object\n1. **{obj.result}**")
       txt = obj.get_data()
-      await message.reply(txt)
+      await message.reply(f"get_data(): **{txt}**")
     except Exception as ex:
-      await message.reply(f"exception: **{type(ex).__name__}**\n2. Error: **{obj.result}**")
+      await message.reply(f"exception: **{type(ex).__name__}**\n2. Last working stage: **{obj.result}**")
     await message.reply("web command issued")
     
 
