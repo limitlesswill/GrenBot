cf69565cd852c009dc5aa27426c465c7201b574e
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 23:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def on_message(message):
       txt = obj.get_data()
       await message.reply(f"get_data(): **{txt}**")
     except Exception as ex:
-      await message.reply(f"exception: **{type(ex).__name__}**\n2. Last working stage: **{obj.result}**")
+      await message.reply(f"exception: **{type(ex).__name__}**\n{str(ex)}\n2. Last working stage: **{obj.result}**")
     await message.reply("web command issued")
     
 
