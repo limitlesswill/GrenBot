a1b9e02c94e8bddafe3c3c8949af98fb5d90ebf2
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 07:04:26 +0200

    Update sweb.py

--- sweb.py
+++ sweb.py
@@ -5,29 +5,34 @@ class hweb:
     self.host = url
     self.port = 80
     self.request = b"GET / HTTP/1.0\r\n\r\n"
-    self.result = "stage constructor"
+    self.result = "instantiating object problem"
     try:
+      self.result = "constructor stage"
       self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.ip = socket.gethostbyname(self.host)
       self.s.connect((self.ip , self.port))
     except:
       self.result = "connection exception"
-    finally:
-      self.result = "connection error"
 
   def get_data(self):
     self.result = "sending request stage"
     try:
+      self.result = "Request Try block"
       self.s.send(self.request)
     except:
       self.result = "Request exception"
 
     reply = b""
     while True:
-      tmp = self.s.recv(80**4)
-      reply += tmp
-      if(len(tmp) <= 0):
-          break
-    self.result = "data"
-    self.s.close()
+      try:
+        tmp = self.s.recv(80**4)
+        reply += tmp
+        if(len(tmp) <= 0):
+            break
+      except:
+        self.result = "data receiving exception"
+    try:
+      self.s.close()
+    except:
+      self.result = "closing socket exception"
     return reply
