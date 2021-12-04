01fbfa53cde52d09fef19cbb87d35807c6d96aad
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 22:14:04 +0200

    Update sweb.py

--- sweb.py
+++ sweb.py
@@ -2,6 +2,7 @@ import socket
 
 class hweb:
   def __init__(self,url="google.com"):
+    self.F = b"F" #General Fail Mark
     self.host = url
     self.port = 80
     self.request = b"GET / HTTP/1.0\r\n\r\n"
@@ -16,14 +17,13 @@ class hweb:
 
   def get_data(self):
     if self.result == "instantiating object problem":
-      return b""
-    self.result = "sending request stage"
+      return self.F
     try:
       self.s.send(self.request)
       self.result = "Request Try block"
     except:
       self.result = "Request exception"
-      return b""
+      return self.F
 
     reply = b""
     try:
@@ -35,12 +35,11 @@ class hweb:
       self.result = "data receiving block"
     except Exception as e:
       self.result = f"data receiving exception {str(e)}"
-      return b""
+      return self.F
     try:
       self.s.close()
       self.result = "closing connection block"
     except:
       self.result = "closing socket exception"
-      return b""
-    self.result = "everything went fine"
+      return self.F
     return reply
