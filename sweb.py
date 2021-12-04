ca3a38cb4da5926c1c2bb0b56b30d2137e3a0dae
Will <48535625+LimitlessWill@users.noreply.github.com>
2021-12-04 06:39:54 +0200

    Update sweb.py

--- sweb.py
+++ sweb.py
@@ -1,35 +1,35 @@
 import socket
 
 class hweb:
-  result = "ok"
   def __init__(self,url="google.com"):
     self.host = url
     self.port = 80
     self.request = b"GET / HTTP/1.0\r\n\r\n"
-    result = "stage constructor"
+    self.result = "ok"
+    self.result = "stage constructor"
     try:
       self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.ip = socket.gethostbyname(self.host)
       self.s.connect((self.ip , self.port))
     except:
-      result = "connection exception"
+      self.result = "connection exception"
     finally:
-      result = "connection error"
+      self.result = "connection error"
 
   def get_data(self):
+      self.result = "sending request stage"
     try:
-      result = "sending request stage"
       self.s.send(self.request)
     except:
-      result = "Request exception"
+      self.result = "Request exception"
     finally:
-      result = "Request finally"
+      self.result = "Request finally"
     reply = b""
     while True:
       tmp = self.s.recv(80**4)
       reply += tmp
       if(len(tmp) <= 0):
           break
-    result = "data"
+    self.result = "data"
     self.s.close()
     return reply
