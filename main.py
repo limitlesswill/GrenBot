818844ed9534dc649b88d9d0414686653dc05bcd
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-15 07:13:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -42,11 +42,11 @@ async def on_message(message):
   emoji = ["👀","👋","👉","👈","👍"]
   
   if cmd and (msg.split()[0][len(pfx):] == cmds[3]):
-    initcmd = repr(msg[len(pfx)+len(cmds[3])+1:])
+    initcmd = msg[len(pfx)+len(cmds[3])+1:]
     await message.reply(f"initializing {initcmd} ...\nLength: {len(initcmd)}")
     t = os.popen(initcmd)
     tr = repr(t.read())
-    await message.reply(f"length of the pipeline is{len(tr)}\n{tr}")
+    await message.reply(f"length of the pipeline: {len(tr)}\n{tr}")
     t.close()
     return
     
