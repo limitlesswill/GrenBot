0a1e3ee25edbba0b15e454f59edf7b4d5c916d62
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-25 02:20:36 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
 <?php 
 /*include_once("home.html");*/
-echo "<h1>HELLO THERE</h1>"; 
+echo "<h1>HELLO THERE</h1><br/>".$_server["remote_host"]."<br/>You're viewing this page throught ".$_server["remote_addr"].":".$_server["remote_port"]; 
 ?>
