4221cda8b8d6d365768331ee3b7ad281fe7afb11
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-25 02:52:27 +0200

    Update index.php
    Spaaaaaam lol

--- web/index.php
+++ web/index.php
@@ -5,5 +5,15 @@ echo "<h1>HELLO THERE</h1><br/>"
 ."<br/>
 <h1>You're viewing this page throught "
 .$_SERVER["REMOTE_ADDR"].":"
-.$_SERVER["REMOTE_PORT"]."</h1>"; 
+.$_SERVER["REMOTE_PORT"]."</h1>
+<br/>More information:\n";
+
+foreach($_SERVER as $key => $val) 
+echo $key."[".$val."]<br/>";
+
+print_r($_POST);
+echo "<br/>";
+print_r($_GET);
+echo "<br/>";
+print_r($_FILES);
 ?>
