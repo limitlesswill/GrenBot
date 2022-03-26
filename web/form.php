be07571b1feef4650ca3541f98375be73042eafa
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-26 10:27:58 +0200

    Update form.php

--- web/form.php
+++ web/form.php
@@ -1,16 +1,42 @@
 <?php
 $title = "";
 $content = "";
+
 $html = '
+<body align ="center" style=
+"color:black;background-color:white;">
+
 <form align="center" method="post"
 onsubmit = "send();return;">
 
-<input required name="title" 
-type="text"/>
-<input required name="content" 
-type="text"/>
+<table>
+<tr>
+<td>Title</td>
+<td>
+<input type="text" name="title" size="25">
+</td>
+</tr>
+<tr>
+<td>Content</td>
+<td>
+<input type="text" name="content" size="25">
+</td>
+</tr> 
+</table>
+<input type="submit" value="Submit"/>
 
 </form>
+
+</body>
 ';
+
+function send()
+{
+global $title , $content;
+echo $title;
+echo "<br/><br/>".$content;
+}
+
 echo $html;
+
 ?>
