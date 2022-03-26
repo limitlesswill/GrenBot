d1230f453ee4d7ba128744a10c0f71fa452d4d72
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-26 03:05:02 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -25,7 +25,12 @@ echo $key."{".$val."}<br/>";
 foreach($_FILES as $key => $val) 
 echo $key."[".$val."]<br/>";
 
-<a href="form.php">Go to The form</a>
-
+<!DOCTYPE HTML>
+<html>
+<head><title>Home Sweet Home</title></head>
+<body>
+<a href="/form.php">Go to The form</a>
+</body>
+</html>
 
 ?>
