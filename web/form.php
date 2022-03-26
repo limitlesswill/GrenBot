50abaefc91b1e614c6b43ca32661db1181a72da9
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-26 10:02:48 +0200

    Create form.php

---
+++ web/form.php
@@ -0,0 +1,12 @@
+<?php
+$title = "";
+$content = "";
+$html = '
+<form align="center" method="post"
+onsubmit = "send();return;">
+<input required name="title" type="text"/>
+<input required content" type="text"/>
+</form>
+';
+echo $html;
+?>
