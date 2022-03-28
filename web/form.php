ef24f8afaf9f7b560217c7f59011adc05080987d
Will <48535625+LimitlessWill@users.noreply.github.com>
2022-03-28 13:51:13 +0200

    Update form.php

--- web/form.php
+++ web/form.php
@@ -49,8 +49,7 @@ if( isset($_POST['title']) )
 $url = $_SERVER['DISCORD_WEBHOOK'];
 $headers = 
 [
-'Content-Type: application/json; 
-charset=utf-8' 
+'Content-Type: application/json; charset=utf-8' 
 ];
 
 $POST = 
