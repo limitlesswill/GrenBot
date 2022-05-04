3248b3675a32f46746ac50805a8e08736df7b2ac
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-05 00:37:14 +0200

    Create a file to handle saving only
    only save and load data .. not to handle the data itself.

---
+++ savefile.py
@@ -0,0 +1,25 @@
+file_name = "settings.ini"
+settings = {"deltime":30,"intval":6}
+
+def save():
+ global file_name
+ global settings
+ file = open(file_name,"w")
+ for key,value in stg.items():
+  file.write(f"{key}:{value}\n")
+ file.close()
+
+def load():
+ global file_name
+ global settings
+ newdic = {}
+ file = open(file_name,"r")
+ for line in file:
+  key=line.split(":")[0]
+  value=line.split(":")[1]
+  newdic[key] = int(value)
+ stg = newdic
+ file.close()
+
+if __name__ == "savefile":
+ print("Done")
