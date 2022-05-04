f56a992bbb848e7b25ef5a3064852d64e638984f
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-05 01:00:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ async def on_message(message):
     await message.add_reaction(emoji[2])
     await message.add_reaction(emoji[3])
     sf.save()
-    await message.reply(sf.peek(),delete_after=sf.settings["deltime"])
     return
 
   if cmd and (msg.split()[0][len(pfx):] not in cmds):
