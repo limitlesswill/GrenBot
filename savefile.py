ee0f95ea4e69e824e2bb9763b249a866f3b7a71b
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-05 02:51:07 +0200

    Update savefile.py

--- savefile.py
+++ savefile.py
@@ -5,7 +5,7 @@ def save():
  global file_name
  global settings
  file = open(file_name,"w")
- for key,value in stg.items():
+ for key,value in settings.items():
   file.write(f"{key}:{value}\n")
  file.close()
 
@@ -18,16 +18,16 @@ def load():
   key=line.split(":")[0]
   value=line.split(":")[1]
   newdic[key] = int(value)
- stg = newdic
+ settings = newdic
  file.close()
 
-def testin(text):
- global file_name
- file = open(file_name,"w")
- return file.write(f"{text}\n")
-def testout():
+def peek():
  global file_name
  file = open(file_name,"r")
- text = file.readlines()
- return str(text)
+ txt = ""
+ for line in file:
+  txt += line
+ file.close()
+ return txt
+
 
