5d436f30ded1f29ca25ce00a038797451334aca5
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-07 05:59:45 +0200

    update
    Trying to upload files into the storage

--- savefile.py
+++ savefile.py
@@ -1,25 +1,31 @@
+import dropbox
+
 file_name = "settings.ini"
 settings = {"deltime":20,"intval":6}
+dbx = dropbox.Dropbox(os.getenv('DROPBOX_TOKEN'))
 
 def save():
+ global dbx
  global file_name
  global settings
  file = open(file_name,"w")
  for key,value in settings.items():
   file.write(f"{key}:{value}\n")
  file.close()
+ dbx.files_upload(file.read(), filename, mute=True)
 
 def load():
+ global dbx
  global file_name
  global settings
+ f, r = dbx.files_download(file_name)
  newdic = {}
- file = open(file_name,"r")
- for line in file:
+ for line in r.content:
   key=line.split(":")[0]
   value=line.split(":")[1]
   newdic[key] = int(value)
  settings = newdic
- file.close()
+ f.close()
 
 def peek():
  global file_name
