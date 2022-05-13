efc0577cd5c873b538a41b65a6d13020b63e78a0
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-13 03:23:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import app_commands
 from discord.ext import tasks
 from os import getenv
 import datetime
-from extra import on_message
+import extra
 
 # Loading TOKEN from environment variables
 TOKEN = getenv('DISCORD_TOKEN')
@@ -18,24 +18,24 @@ CORNJOB_CHANNEL_ID = 971240750731890738
 
 
 class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents, application_id: int):
-        super().__init__(intents=intents, application_id=application_id)
-        # A CommandTree is a special type that holds all the application command
-        # state required to make it work. This is a separate class because it
-        # allows all the extra state to be opt-in.
-        # Whenever you want to work with application commands, your tree is used
-        # to store and work with them.
-        # Note: When using commands.Bot instead of discord.Client, the bot will
-        # maintain its own tree instead.
-        self.tree = app_commands.CommandTree(self)
+ def __init__(self, *, intents: discord.Intents, application_id: int):
+ super().__init__(intents=intents, application_id=application_id)
+ # A CommandTree is a special type that holds all the application command
+ # state required to make it work. This is a separate class because it
+ # allows all the extra state to be opt-in.
+ # Whenever you want to work with application commands, your tree is used
+ # to store and work with them.
+ # Note: When using commands.Bot instead of discord.Client, the bot will
+ # maintain its own tree instead.
+ self.tree = app_commands.CommandTree(self)
 
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+ # In this basic example, we just synchronize the app commands to one guild.
+ # Instead of specifying a guild to every command, we copy over our global commands instead.
+ # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+ async def setup_hook(self):
+ # This copies the global commands over to your guild.
+ self.tree.copy_global_to(guild=MY_GUILD)
+ await self.tree.sync(guild=MY_GUILD)
 
 
 intent = discord.Intents.default()
@@ -51,33 +51,33 @@ client = MyClient(intents=intent, application_id=APP_ID)
 # A cornjob loops every 1 minute (get time send it in a specific channel)
 @tasks.loop(minutes=1)
 async def test():
-    channel = client.get_channel(CORNJOB_CHANNEL_ID)
-    cur = datetime.datetime.utcnow().strftime("\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚  %I:%M  %p  \💚")
-    await channel.send(f"\t\t\t\t\t**{cur}**",delete_after=59)
+ channel = client.get_channel(CORNJOB_CHANNEL_ID)
+ cur = datetime.datetime.utcnow().strftime("\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚  %I:%M  %p  \💚")
+ await channel.send(f"\t\t\t\t\t**{cur}**",delete_after=59)
 
 
 @client.event
 async def on_ready():
-    print(f"{client.user} has connected to Discord!\nHello World")
-    test.start()
-    print('------')
-    print("test function has started")
+ print(f"{client.user} has connected to Discord!\nHello World")
+ test.start()
+ print('------')
+ print("test function has started")
 
 
 @client.tree.command()
 async def hello(interaction: discord.Interaction):
-    """Says hi!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+ """Says hi!"""
+ await interaction.response.send_message(f'Hi, {interaction.user.mention}')
 
 
 @client.tree.command()
 @app_commands.describe(
-    first_value='The first value you want to add something to',
-    second_value='The value you want to add to the first value')
+ first_value='The first value you want to add something to',
+ second_value='The value you want to add to the first value')
 
 async def multi(interaction: discord.Interaction, first_value: int, second_value: int):
-    """Multiplying two numbers together."""
-    await interaction.response.send_message(f'{first_value} * {second_value} = {first_value * second_value}')
+ """Multiplying two numbers together."""
+ await interaction.response.send_message(f'{first_value} * {second_value} = {first_value * second_value}')
 
 
 # The rename decorator allows us to change the display of the parameter on Discord.
@@ -87,8 +87,8 @@ async def multi(interaction: discord.Interaction, first_value: int, second_value
 @app_commands.rename(text_to_send='text')
 @app_commands.describe(text_to_send='Text to send in the current channel')
 async def send(interaction: discord.Interaction, text_to_send: str):
-    """Sends the text into the current channel."""
-    await interaction.response.send_message(text_to_send)
+ """Sends the text into the current channel."""
+ await interaction.response.send_message(text_to_send)
 
 
 # To make an argument optional, you can either give it a supported default argument
@@ -96,47 +96,17 @@ async def send(interaction: discord.Interaction, text_to_send: str):
 @client.tree.command()
 @app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
 async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
-    """Says when a member joined."""
-    # If no member is explicitly provided then we use the command user here
-    member = member or interaction.user
+ """Says when a member joined."""
+ # If no member is explicitly provided then we use the command user here
+ member = member or interaction.user
 
-    # The format_dt function formats the date time into a human readable representation in the official client
-    await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
-
-
-# A Context Menu command is an app command that can be run on a member or on a message by
-# accessing a menu within the client, usually via right clicking.
-# It always takes an interaction as its first parameter and a Member or Message as its second parameter.
-
-# This context menu command only works on members
-@client.tree.context_menu(name='Show Join Date')
-async def show_join_date(interaction: discord.Interaction, member: discord.Member):
-    # The format_dt function formats the date time into a human readable representation in the official client
-    await interaction.response.send_message(f'{member} joined at {discord.utils.format_dt(member.joined_at)}')
+ # The format_dt function formats the date time into a human readable representation in the official client
+ await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
 
 
-# This context menu command only works on messages
-@client.tree.context_menu(name='Report to Moderators')
-async def report_message(interaction: discord.Interaction, message: discord.Message):
-    # We're sending this response message with ephemeral=True, so only the command executor can see it
-    await interaction.response.send_message(
-        f'Thanks for reporting this message by {message.author.mention} to our moderators.', ephemeral=True
-    )
-
-    # Handle report by sending it into a log channel
-    log_channel = interaction.guild.get_channel(LOG_CHANNEL)  # replace with your channel id
-
-    embed = discord.Embed(title='Reported Message')
-    if message.content:
-        embed.description = message.content
-
-    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-    embed.timestamp = message.created_at
-
-    url_view = discord.ui.View()
-    url_view.add_item(discord.ui.Button(label='Go to Message', style=discord.ButtonStyle.url, url=message.jump_url))
-
-    await log_channel.send(embed=embed, view=url_view)
+@client.event()
+async def on_message(message):
+ await extra.on_message(message)
 
 
 client.run(TOKEN)
