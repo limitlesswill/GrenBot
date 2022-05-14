3f767435fa012bbc7941dc07f1d81d4b33541847
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-14 04:37:06 +0200

    Update cornjobs.py

--- cornjobs.py
+++ cornjobs.py
@@ -1,14 +1,9 @@
 from discord.ext import tasks
 import datetime
+import discord
+from base import client
 
 ID_CHANNEL_CORNJOB = 971240750731890738
-
-@client.event
-async def on_ready():
- print(f"{client.user} has connected to Discord!\nHello World")
- test.start()
- print('------')
- print("test function has started")
 
 
 # A cornjob loops every 1 minute (get time send it in a specific channel)
@@ -17,3 +12,11 @@ async def test():
  channel = client.get_channel(ID_CHANNEL_CORNJOB)
  cur = datetime.datetime.utcnow().strftime("\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚  %I:%M  %p  \💚")
  await channel.send(f"\t\t\t\t\t**{cur}**",delete_after=59)
+
+
+@client.event
+async def on_ready():
+ print(f"{client.user} has connected to Discord!\nHello World")
+ test.start()
+ print('------')
+ print("test function has started")
