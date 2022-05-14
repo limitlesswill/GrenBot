dd00353f53ccacb29f31e311c07976fda1e635fe
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-14 06:17:53 +0200

    Update cornjob.py

--- cornjob.py
+++ cornjob.py
@@ -1,16 +1,27 @@
 from discord.ext import tasks
-import datetime
+from datetime import datetime
 import discord
 from base import client
 
 ID_CHANNEL_CORNJOB = 971240750731890738
 
+"""
+# ready to convert them into other calendars
+import datetime
+time = datetime.datetime.utcnow()
+year = int(time.strftime("%Y"))+5
+month = time.strftime("%m")
+day = time.strftime("%d")
+hour = time.strftime("%H")
+minute = time.strftime("%M")
+second = time.strftime("%S")
+"""
 
 # A cornjob loops every 1 minute (get time send it in a specific channel)
 @tasks.loop(minutes=1)
 async def test():
  channel = client.get_channel(ID_CHANNEL_CORNJOB)
- cur = datetime.datetime.utcnow().strftime("\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚  %I:%M  %p  \💚")
+ cur = datetime.utcnow().strftime("\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚  %I:%M  %p  \💚")
  await channel.send(f"\t\t\t\t\t**{cur}**",delete_after=59)
 
 
