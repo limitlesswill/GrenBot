9f06ddb29ff3cd167a4dde7539bec44063078649
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-14 04:46:42 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from os import getenv
+from base import client
+from message import on_message
+from cornjob import on_ready , test
+
+
+# Loading TOKEN from environment variables
+TOKEN = getenv('DISCORD_TOKEN')
+
+
+client.run(TOKEN)
