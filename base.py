27f2301670e0abfa0b14f3c8be668e09d4afa281
Will <48535625+limitlesswill@users.noreply.github.com>
2022-05-14 04:13:33 +0200

    Update and rename main.py to base.py

--- base.py
+++ base.py
@@ -67,45 +67,4 @@ async def on_ready():
  print("test function has started")
 
 
-@client.tree.command()
-async def hello(interaction: discord.Interaction):
- """Says hi!"""
- await interaction.response.send_message(f'Hi, {interaction.user.mention}')
-
-
-@client.tree.command()
-@app_commands.describe(
- first_value='The first value you want to add something to',
- second_value='The value you want to add to the first value')
-
-async def multi(interaction: discord.Interaction, first_value: int, second_value: int):
- """Multiplying two numbers together."""
- await interaction.response.send_message(f'{first_value} * {second_value} = {first_value * second_value}')
-
-
-# The rename decorator allows us to change the display of the parameter on Discord.
-# In this example, even though we use `text_to_send` in the code, the client will use `text` instead.
-# Note that other decorators will still refer to it as `text_to_send` in the code.
-@client.tree.command()
-@app_commands.rename(text_to_send='text')
-@app_commands.describe(text_to_send='Text to send in the current channel')
-async def send(interaction: discord.Interaction, text_to_send: str):
- """Sends the text into the current channel."""
- await interaction.response.send_message(text_to_send)
-
-
-# To make an argument optional, you can either give it a supported default argument
-# or you can mark it as Optional from the typing standard library. This example does both.
-@client.tree.command()
-@app_commands.describe(member='The member you want to get the joined date from; defaults to the user who uses the command')
-async def joined(interaction: discord.Interaction, member: Optional[discord.Member] = None):
- """Says when a member joined."""
- # If no member is explicitly provided then we use the command user here
- member = member or interaction.user
- # The format_dt function formats the date time into a human readable representation in the official client
- await interaction.response.send_message(f'{member} joined {discord.utils.format_dt(member.joined_at)}')
-
-
-
-
 
