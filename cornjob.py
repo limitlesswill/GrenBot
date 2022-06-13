3ec9eeebecb461d732eedbc63085a1070048eb94
Will <48535625+limitlesswill@users.noreply.github.com>
2022-06-13 23:53:38 +0200

    Big Update
    Adding GrenDate

--- cornjob.py
+++ cornjob.py
@@ -1,32 +1,23 @@
 from discord.ext import tasks
-from datetime import datetime
 from base import client
+import grendate
 
 ID_CHANNEL_CORNJOB = 971240750731890738
-
-"""
-# ready to convert them into other calendars
-import datetime
-time = datetime.datetime.utcnow()
-year = int(time.strftime("%Y"))+5
-month = time.strftime("%m")
-day = time.strftime("%d")
-hour = time.strftime("%H")
-minute = time.strftime("%M")
-second = time.strftime("%S")
-"""
 
 # A cornjob loops every 1 minute (get time send it in a specific channel)
 @tasks.loop(minutes=1)
 async def test():
  channel = client.get_channel(ID_CHANNEL_CORNJOB)
- cur = datetime.utcnow().strftime("\n\n\t\t\t\t\t    %Y/%B/%d\n\n\t\t\t\t\t\💚    %I:%M  %p    \💚")
- await channel.send(f"\t\t\t\t\t**{cur}**",delete_after=59)
+ year,month,day,month_name,hour,minute,meridiem = GrenDate().now()
+ season = GrenDate(year,month,day).season()
+ cur = f"\n\n\t\t\t\t\t    {year}/{month_name}/{day}\n\n\t\t\t\t\t\💚    {hour}:{minute}  {meridiem}    \💚")
+ await channel.send(f"\t\t\t\t\t**{cur}**\n\t\t\t\t\t{season}",delete_after=59)
 
 
 @client.event
 async def on_ready():
  print(f"{client.user} has connected to Discord!\nHello World")
- await test.start()
  print('------')
- print("test function has started")
+ print("test function is starting ...")
+ await test.start()
+
