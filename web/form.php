e9d3c4abd6cd6c2d699bd0fbc951a75e82b16750
Will <48535625+limitlesswill@users.noreply.github.com>
2022-08-23 00:12:37 +0200

    removing quotes from those variables

--- web/form.php
+++ web/form.php
@@ -85,9 +85,9 @@ curl_exec($ch);
 */
 if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token']))
 {
-echo json_encode($_GET['hub_challenge']);
+echo $_GET['hub_challenge'];
 }else{
-echo json_encode("Go away.");
+echo "Go away.";
 echo $html;
 }
 
