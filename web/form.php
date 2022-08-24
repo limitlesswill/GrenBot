2216ccac5ea9f2c0dd9009f8ccab1c77fbd8c022
Will <48535625+limitlesswill@users.noreply.github.com>
2022-08-24 08:24:36 +0200

    Update form.php

--- web/form.php
+++ web/form.php
@@ -71,7 +71,7 @@ return curl_exec($ch);
 if (isset($_GET['hub_mode']) && isset($_GET['hub_challenge']) && isset($_GET['hub_verify_token']))
 {
 echo $_GET['hub_challenge'];
-}else if(isset($_POST['value']['item']))
+}else if($_POST['value']['from']['id'] != $_SERVER['fb_page_id'])
 {
 $name = $_POST['value']['from']['name'];
 $comment_id = $_POST['value']['comment_id'];
@@ -80,6 +80,14 @@ $url = 'https://graph.facebook.com/v14.0/'.$comment_id.'/comments';
 $fb_payload = 
 ['access_token' => $_SERVER['fb_token'],
 'message' => $msg];
+$dc = $_SERVER['DISCORD_WEBHOOK'];
+$dc_payload = 
+[ 
+'username' => $name,
+'avatar_url' => 'https://scontent.fcai1-3.fna.fbcdn.net/v/t39.8562-6/109960336_274477960450922_1306319190754819753_n.png?_nc_cat=107&ccb=1-7&_nc_sid=6825c5&_nc_eui2=AeEW0Tv6csstYDgEbtnMu-g4JxDCgWesWeInEMKBZ6xZ4jN15myTe-sJn1pUwiWyt2YnTf0E3QM3nWkTaegX1JNZ&_nc_ohc=creb8yK0R18AX-xUJZ5&_nc_ht=scontent.fcai1-3.fna&oh=00_AT8xmtU5v_S4xoW_zAaW9OWXh3wjta-Qk79nNkCqbXb_ow&oe=630A31B0',
+'content' => $msg
+];
+sendit($dc,$dc_payload);
 echo sendit($url,$fb_payload);
 echo 200;
 }else if($_SERVER['REQUEST_METHOD'] === 'POST')
