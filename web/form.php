df2b0eea194fc543becf83bd81d8de898c100cfd
Will <48535625+limitlesswill@users.noreply.github.com>
2022-08-24 19:53:12 +0200

    Update form.php

--- web/form.php
+++ web/form.php
@@ -80,15 +80,15 @@ $comment_id = $data['entry']['changes']['value']['comment_id'];
 $url = 'https://graph.facebook.com/v14.0/'.$comment_id.'/comments';
 $msg = 'name\n'.$name.'\nid\n'.$comment_id.'\nurl\n'.$url.'\nmessage\n'.$data['entry']['changes']['value']['message'];
 $fb_payload = ['access_token' => $_SERVER['fb_token'],'message' => $msg];
+$roro = sendit($url,$fb_payload);
 $dc = $_SERVER['DISCORD_WEBHOOK'];
 $dc_payload = 
 [ 
 'username' => 'Facebook',
 'avatar_url' => 'https://scontent.fcai1-3.fna.fbcdn.net/v/t39.8562-6/109960336_274477960450922_1306319190754819753_n.png?_nc_cat=107&ccb=1-7&_nc_sid=6825c5&_nc_eui2=AeEW0Tv6csstYDgEbtnMu-g4JxDCgWesWeInEMKBZ6xZ4jN15myTe-sJn1pUwiWyt2YnTf0E3QM3nWkTaegX1JNZ&_nc_ohc=creb8yK0R18AX-xUJZ5&_nc_ht=scontent.fcai1-3.fna&oh=00_AT8xmtU5v_S4xoW_zAaW9OWXh3wjta-Qk79nNkCqbXb_ow&oe=630A31B0',
-'content' => file_get_contents('php://input')
+'content' => '**Raw Data**\n'.file_get_contents('php://input').'\n**End**\n'.$roro
 ];
 sendit($dc,$dc_payload);
-sendit($url,$fb_payload);
 echo 200;
 }else{
 echo $html;
